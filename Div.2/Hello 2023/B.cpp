bb2eca3b4377388e2379b332a943ff28083ec984
kss418 <108602943+kss418@users.noreply.github.com>
2024-09-19 02:37:26 +0900

    Create B.cpp

---
+++ Div.2/Hello 2023/B.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n == 3) {
+            cout << "No" << "\n";
+            continue;
+        }
+        
+        cout << "YES" << "\n";
+        if (n % 2) {
+            n--;
+            int cur = n / 2;
+            for (int i = 1; i <= cur; i++) {
+                cout << cur - 1 << " " << -cur << " ";
+            }
+            cout << cur - 1 << "\n";
+
+        }
+        else {
+            for (int i = 1; i <= n; i++) {
+                if (i % 2) {
+                    cout << 1 << " ";
+                }
+                else {
+                    cout << -1 << " ";
+                }
+            }
+            cout << "\n";
+        }
+
+
+    }
+    
+
+
+
+   
+    return 0;
+}
